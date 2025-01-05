d0d6af2751c289f589ce7bd93da5a406bbd00ea2
talles <talles.d.o.p@gmail.com>
2025-01-05 07:23:55 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*',
+  },
+};
+
+const nms = new NodeMediaServer(config);
+nms.run();
+
+console.log('RTMP server is running on port 1935');
