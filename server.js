909b3e77ad52a9fab8dfc8f8d4088bdc55302353
talles <talles.d.o.p@gmail.com>
2025-01-05 07:42:38 -0300

    new

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const NodeMediaServer = require('node-media-server');
+const express = require('express');
+const path = require('path');
 
+// RTMP Server Configuration
 const config = {
   rtmp: {
     port: 1935,
@@ -12,9 +15,35 @@ const config = {
     port: 8000,
     allow_origin: '*',
   },
+  trans: {
+    ffmpeg: '/usr/local/bin/ffmpeg', // Path to FFmpeg
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+      },
+    ],
+  },
 };
 
+// Start the RTMP Server
 const nms = new NodeMediaServer(config);
 nms.run();
 
-console.log('RTMP server is running on port 1935');
+// HTTP Server to Serve the HTML Page
+const app = express();
+const PORT = 3000;
+
+// Serve Static Files from the "public" Directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the HTML Page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the HTTP Server
+app.listen(PORT, () => {
+  console.log(`HTTP server running on http://localhost:${PORT}`);
+});
